baedf2a9e6f0ce6fabd4de0a5a70ef8bee59b707
sent7890 <dhxurui@qq.com>
2024-11-14 17:46:21 +0800

    Delete src directory

--- src/MD5.h
+++
@@ -1,9 +0,0 @@
-#ifndef MD5_H  
-#define MD5_H  
-
-#define MD5FUNC		__stdcall
-
-void MD5FUNC MakeMD5(unsigned char* inputBytes, int byteLen, unsigned char digest[16]);
-void MD5FUNC MakeMD5Hex(unsigned char digest[16], char* hexStr);
-
-#endif
